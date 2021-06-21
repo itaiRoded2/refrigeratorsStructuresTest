5e6c0cf2db8974d415301c1ee8eb8380a0d2f97f
itaiRoded <itai.roded@gmail.com>
2021-06-21 18:45:13 -0500

    ready to add enums

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfigOptionsComponent } from './components/config-options/config-options.component';
 import { TheTaskComponent } from './components/the-task/the-task.component';
 
 const routes: Routes = [
 
   { path: '', component: TheTaskComponent, runGuardsAndResolvers: "always" },
+  { path: 'configurations', component: ConfigOptionsComponent, runGuardsAndResolvers: "always" },
 
 ];
 
 @NgModule({
   imports: [
-      RouterModule.forRoot(routes, {
+    RouterModule.forRoot(routes, {
       })
   ],
   exports: [RouterModule]
