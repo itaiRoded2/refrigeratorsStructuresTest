64aa559bd302a56fa87cc7fc1ccf6ab13eb81fb5
itaiRoded <itai.roded@gmail.com>
2021-06-21 19:44:16 -0500

    added a 2 fridge for tetsing and we see different results for same call

--- src/app/components/config-options/config-options.component.ts
+++ src/app/components/config-options/config-options.component.ts
@@ -16,21 +16,32 @@ export class ConfigOptionsComponent implements OnInit {
     let sampleFridge = this.createFridge()
     let whereIsTheMilk = sampleFridge.findMyFood("milk");
     let whereIsTheGouda = sampleFridge.findMyFood("Gouda");
-
-    debugger;
     let whereAreTheVeggies = sampleFridge.findMyFood("veggies");
 
 
-    //alert("where Is The Milk for our sample fridge (see code): " + whereIsTheMilk);
-    //alert("where Is The Gouda for our sample fridge (see code): " + whereIsTheGouda);
-    alert("where are The veggies for our sample fridge (see code): " + whereAreTheVeggies);
+    // alert("where Is The Milk for our sample fridge (see code): " + whereIsTheMilk);
+    // alert("where Is The Gouda for our sample fridge (see code): " + whereIsTheGouda);
+    // alert("where are The veggies for our sample fridge (see code): " + whereAreTheVeggies);
+
+    let sampleFridge2 = this.createFridge2()
+    let whereIsTheMilk2 = sampleFridge2.findMyFood("milk");
+    let whereIsTheGouda2 = sampleFridge2.findMyFood("Gouda");
+    let whereAreTheVeggies2 = sampleFridge2.findMyFood("veggies");
+
+
+    //alert("where Is The Milk for our sample fridge (see code): " + whereIsTheMilk2);
+    //alert("where Is The Gouda for our sample fridge (see code): " + whereIsTheGouda2);
+     alert("where are The veggies for our sample fridge (see code): " + whereAreTheVeggies2);
 
   }
 
   createFridge() : Fridge {
     let fridge: Fridge = new Fridge(FridgeType.SideBySide, FridgeSizes._30By36,2,3 )
-
     return fridge;
   }
 
+  createFridge2() : Fridge {
+    let fridge: Fridge = new Fridge(FridgeType.TopFreezer, FridgeSizes._30By36,2,3 )
+    return fridge;
+  }
 }
