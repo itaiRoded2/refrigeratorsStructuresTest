5f9cb905d21c10f66fdc3cd8e949ac5853bcb1f6
itaiRoded <itai.roded@gmail.com>
2021-06-21 19:58:21 -0500

    done

--- src/app/components/config-options/config-options.component.ts
+++ src/app/components/config-options/config-options.component.ts
@@ -9,25 +9,30 @@ import { FridgeSizes, FridgeType } from './ConfigOptions';
 })
 export class ConfigOptionsComponent implements OnInit {
 
+  fridge1: Fridge;
+  fridge2: Fridge;
+
   constructor() { }
 
   ngOnInit(): void {
 
-    let sampleFridge = this.createFridge()
-    let whereIsTheMilk = sampleFridge.findMyFood("milk");
-    let whereIsTheGouda = sampleFridge.findMyFood("Gouda");
-    let whereAreTheVeggies = sampleFridge.findMyFood("veggies");
+    this.fridge1 = this.createFridge();
+    this.fridge2 = this.createFridge2();
 
+    // let sampleFridge = this.createFridge()
+
+    // let whereIsTheMilk = sampleFridge.findMyFood("milk");
+    // let whereIsTheGouda = sampleFridge.findMyFood("Gouda");
+    // let whereAreTheVeggies = sampleFridge.findMyFood("veggies");
 
     // alert("where Is The Milk for our sample fridge (see code): " + whereIsTheMilk);
     // alert("where Is The Gouda for our sample fridge (see code): " + whereIsTheGouda);
     // alert("where are The veggies for our sample fridge (see code): " + whereAreTheVeggies);
 
-    let sampleFridge2 = this.createFridge2()
-    let whereIsTheMilk2 = sampleFridge2.findMyFood("milk");
-    let whereIsTheGouda2 = sampleFridge2.findMyFood("Gouda");
-    let whereAreTheVeggies2 = sampleFridge2.findMyFood("veggies");
-
+    // let sampleFridge2 = this.createFridge2()
+    // let whereIsTheMilk2 = sampleFridge2.findMyFood("milk");
+    // let whereIsTheGouda2 = sampleFridge2.findMyFood("Gouda");
+    // let whereAreTheVeggies2 = sampleFridge2.findMyFood("veggies");
 
     //alert("where Is The Milk for our sample fridge (see code): " + whereIsTheMilk2);
     //alert("where Is The Gouda for our sample fridge (see code): " + whereIsTheGouda2);
@@ -44,4 +49,36 @@ export class ConfigOptionsComponent implements OnInit {
     let fridge: Fridge = new Fridge(FridgeType.TopFreezer, FridgeSizes._30By36,2,3 )
     return fridge;
   }
+
+  findMilkFridge1() {
+    let whereIsTheMilk = this.fridge1.findMyFood("milk");
+    alert("where Is The Milk for our sample fridge (see code):\n" + whereIsTheMilk);
+  }
+
+  findMilkFridge2() {
+    let whereIsTheMilk = this.fridge2.findMyFood("milk");
+    alert("where Is The Milk for our sample fridge (see code):\n" + whereIsTheMilk);
+  }
+
+  findGouda1() {
+    let whereAmI = this.fridge1.findMyFood("Gouda");
+    alert("where Is The Gouda for our sample fridge (see code):\n" + whereAmI);
+  }
+
+  findGouda2() {
+    let whereAmI = this.fridge2.findMyFood("Gouda");
+    alert("where Is The Gouda for our sample fridge (see code):\n" + whereAmI);
+  }
+
+  findVeggies1() {
+    let whereAmI = this.fridge1.findMyFood("veggies");
+    alert("where are The veggies for our sample fridge (see code):\n" + whereAmI);
+  }
+
+  findVeggies2() {
+    let whereAmI = this.fridge2.findMyFood("veggies");
+    alert("where are The veggies for our sample fridge (see code):\n" + whereAmI);
+  }
+
+
 }
